85c76ff9aec73aa8a813389383daa6931ea418ac
thales <thalesdaniel2013@gmail.com>
2023-02-18 00:39:20 -0300

    feat: finish the input todo

--- src/app/modules/pages/home/components/todo-input/todo-input.component.ts
+++ src/app/modules/pages/home/components/todo-input/todo-input.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-todo-input",
   templateUrl: "./todo-input.component.html",
   styleUrls: ["./todo-input.component.css"],
 })
-export class TodoInputComponent {
+export class TodoInputComponent implements OnInit, OnDestroy {
   data = new Date();
+  timer: any = null!;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => (this.data = new Date()), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
 }
